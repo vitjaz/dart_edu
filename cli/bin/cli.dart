89579eb3d1aaac4e5bd786c98c37855a110b580c
Vitaly <jameshudsonrandall@gmail.com>
2026-06-18 19:37:40 +0300

    Write Async Code
    - Проведена работа в асинхронностью
    - Изучен тип Future, ключевые слова async/await
    - Обнаружено колоссально много похожего между JS и Dart
    - Использован пакет http, чтобы выполнять HTTP запросы

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
@@ -7,7 +8,7 @@ void main(List<String> arguments) {
     print('Hello, Dart!');
   } else if (arguments.first == 'version') {
     print('DartPedia cli version $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wiki') {
     // final - переменная назначается 1 раз и в последствии не может быть изменена
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
@@ -16,23 +17,48 @@ void main(List<String> arguments) {
   }
 }
 
-void searchWikipedia(List<String>? arguments) {
+void searchWikipedia(List<String>? arguments) async {
   final String articleTitle;
 
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFromStdin = stdin.readLineSync();
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return;
+    }
+    articleTitle = inputFromStdin;
   } else {
     articleTitle = arguments.join(' ');
   }
 
   print('Looking up articles about "$articleTitle". Please wait...');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+
+  final articleContent = await getWikiArticle(articleTitle);
+
+  print(articleContent);
 }
 
 void printMessage() {
   print(
-    "The following command are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+    "The following command are valid: 'help', 'wiki', 'search <ARTICLE-TITLE>'",
   );
+}
+
+// Тип Future<String> говорит нам о том, что функция вернет строку, но не немедленно, а в когда то в будущем
+// Легко провести аналогию с TS, где мы бы написали Promise<string> - то есть обещание о том, что когда-то мы вернем строку
+Future<String> getWikiArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  // интерполяция свойств объекта происходит через $ и фигурные скобки
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
